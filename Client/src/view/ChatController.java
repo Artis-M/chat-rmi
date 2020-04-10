862e2b58a30322d08e5239f88f6302621f3ac885
Artis <mundurs.artis@gmail.com>
2020-04-10 16:14:41 +0200

    slight cleanup

--- Client/src/view/ChatController.java
+++ Client/src/view/ChatController.java
@@ -1,7 +1,6 @@
 package view;
 
 import ViewModel.ChatViewModel;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
